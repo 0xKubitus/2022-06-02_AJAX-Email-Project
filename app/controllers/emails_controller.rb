df3fa8da5fa063af4484abdc3d7dcd7143da08dc
0xKubitus <n.cubilier@gmail.com>
2022-06-02 18:51:36 +0200

    petite sauvegarde

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -8,6 +8,12 @@ class EmailsController < ApplicationController
 
   # GET /emails/1 or /emails/1.json
   def show
+    @email = Email.find(params[:id])
+
+    respond_to do |format|
+      format.html { }
+      format.js { }
+    end
   end
 
   # GET /emails/new
