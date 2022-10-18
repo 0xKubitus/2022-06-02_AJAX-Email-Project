0a5826b71199b918be8884be79f102ab814695d1
0xKubitus <n.cubilier@gmail.com>
2022-10-18 17:14:16 +0200

    removed useless stuff that was copied from previous project(to-do-list project)

--- db/schema.rb
+++ db/schema.rb
@@ -15,28 +15,11 @@ ActiveRecord::Schema.define(version: 2022_06_02_100558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "emails", force: :cascade do |t|
     t.string "object"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title"
-    t.boolean "status", default: false
-    t.datetime "deadline"
-    t.bigint "category_id"
-    t.string "image"
-    t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-3.times do
-  my_category = Category.create(title: Faker::Book.genre)
-  5.times do
-    my_task = Task.new(title: Faker::Book.title,
-                      deadline: Faker::Date.forward(days: 23),
-                      image: Faker::Avatar.image)
-    my_task.category = my_category
-    my_task.save
-  end
-end
+Email.destroy_all
 
 
-20.times do
+10.times do
   my_email = Email.create(object: Faker::Lorem.sentence(word_count: 1, supplemental: true, random_words_to_add: 4) , body: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 10))
 end
