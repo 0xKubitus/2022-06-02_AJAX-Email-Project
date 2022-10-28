07cc232da3536bbf6e17a827c4fb1861f2735c8d
0xKubitus <n.cubilier@gmail.com>
2022-10-28 14:43:22 +0200

    'read' attribute added to Email model for Part 2.6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_160540) do
+ActiveRecord::Schema.define(version: 2022_10_28_123733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_10_24_160540) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
   end
 
 end
